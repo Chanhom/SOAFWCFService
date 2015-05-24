9bd9d671ba2d4364be07e77bb73d66e6750b293f
chanhom <chanhomm@gmail.com>
2015-05-24 10:20:09 +0700

    First Update

---
+++ WCFServiceTest/IService1.vb
@@ -0,0 +1,34 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the interface name "IService1" in both code and config file together.
+<ServiceContract(Namespace:="http://Microsoft.ServiceModel.WCFServiceTest")>
+Public Interface IService1
+    <OperationContract()>
+    Function HelloWorld() As String
+
+
+    <OperationContract()>
+    Function GetUserObject(ByVal pUserCode As String) As WCFUser
+
+    <OperationContract()>
+    Function GetAllUser() As List(Of WCFUser)
+    ' TODO: Add your service operations here
+
+End Interface
+
+' Use a data contract as illustrated in the sample below to add composite types to service operations.
+
+
+
+<DataContract()>
+Public Class WCFUser
+    <DataMember()>
+    Public Property UserCode As String
+    <DataMember()>
+    Public Property FirstName As String
+    <DataMember()>
+    Public Property LastName As String
+    <DataMember()>
+    Public Property Age As Integer
+    <DataMember()>
+    Public Property IsActive As Boolean
+
+End Class
---
+++ WCFServiceTest/Service1.svc.vb
@@ -0,0 +1,57 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the class name "Service1" in code, svc and config file together.
+' NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.vb at the Solution Explorer and start debugging.
+Public Class Service1
+    Implements IService1
+
+    Public Sub New()
+    End Sub
+
+    Public Function HelloWorld() As String Implements IService1.HelloWorld
+        Return "Hello WCF Service"
+    End Function
+
+    Public Function GetUserObject(ByVal pUserCode As String) As WCFUser Implements IService1.GetUserObject
+        Try
+            Return New WCFUser With {.UserCode = "xxxx", .FirstName = ".NET Develper", .LastName = "WCF Service", .Age = 27, .IsActive = True}
+        Catch ex As Exception
+            Return Nothing
+        End Try
+    End Function
+
+    Public Function GetAllUser() As List(Of WCFUser) Implements IService1.GetAllUser
+        Try
+            Dim objUser As WCFUser
+            Dim ListUser As New List(Of WCFUser)
+
+
+            objUser = New WCFUser
+            objUser.FirstName = "Sompetch"
+            objUser.LastName = "Chanhom"
+            objUser.Age = 27
+            objUser.IsActive = True
+            ListUser.Add(objUser)
+
+
+
+            objUser = New WCFUser
+            objUser.FirstName = "Tankhun"
+            objUser.LastName = "Chanhom"
+            objUser.Age = 2
+            objUser.IsActive = True
+            ListUser.Add(objUser)
+
+
+            objUser = New WCFUser
+            objUser.FirstName = "Thanongsak"
+            objUser.LastName = "Chanhom"
+            objUser.Age = 30
+            objUser.IsActive = True
+            ListUser.Add(objUser)
+
+            Return ListUser
+        Catch ex As Exception
+            Return Nothing
+        End Try
+    End Function
+
+End Class
